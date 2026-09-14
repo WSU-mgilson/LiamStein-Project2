3377c9cbf463f6e870c08a66d67a782d3f38497c
RogueMemer <liambzstein@gmail.com>
2026-09-14 12:31:12 -0400

    Implement if-else logic in assembly code

--- ifelse.asm
+++ ifelse.asm
@@ -1,21 +1,26 @@
 .ORIG x3000
+
 R0_VAL .FILL 6
 R1_VAL .FILL 9
 RESULT_ADDR .FILL x8002
+
 LD R0, R0_VAL
 LD R1, R1_VAL
 NOT R2, R1
 ADD R2, R2, #1
 ADD R2, R0, R2       ; R2 = R0 -R1
+
 BRz IF_EQUAL
 
 IF_EQUAL:
     AND R3, R3, #0
     ADD R3, R3, #5       ; R3 = 5
     BRNZP END_IFELSE
+
 ELSE_BLOCK:
     AND R3, R3, #0
     ADD R3, R3, #-5       ; R3 = -5
+
 END_IFELSE:
     ST R3, RESULT_ADDR
     HALT
