ca1524ed97425d828e3b9257458156a32b86fcf2
RogueMemer <liambzstein@gmail.com>
2026-09-17 16:37:28 -0400

    Update loop and multiplication structure in exponent.asm
    Refactor loop and multiply logic for clarity and correctness.

--- exponent.asm
+++ exponent.asm
@@ -1,21 +1,19 @@
 .ORIG x3000
 LD R1, X
 LD R2, Y
-AND R2, R2 #0
-ADD R2, R2 #1
+AND R2, R2, #0
+ADD R2, R2, #1
 
-LOOP
-  BRz DONE
+LOOP_CHECK BRz DONE
   ADD R1, R1, #0
   ADD R4, R2, #0
 
-MULTIPLY
-  ADD R3, R3, #0
-  ADD R4, R2, #0
-  BRq MULTIPLY
+MULTIPLY  ADD R3, R3, R0
+  ADD R4, R4, #-1
+  BRp MULTIPLY
   ADD R2, R3, R0
   ADD R1, R1, #-1
-  BNnzp LOOP
+  BRp LOOP_CHECK
 DONE
   STI R2, RESULT
   HALT
@@ -26,5 +24,5 @@ RESULT .FILL x8000
 
 .END
 
-
+; edited this inside of LC-3, so thats why it's a copy paste.
 ; refrence: https://www.cs.utexas.edu/~fussell/courses/cs310h/lectures/ISA.pdf
